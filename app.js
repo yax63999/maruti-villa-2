ff942d05b65d7205e0b7b64c9c88990cf359ec62
yax63999 <135575682+yax63999@users.noreply.github.com>
2025-10-01 20:05:26 +1300

    Updated app.js to use dynamic port

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
